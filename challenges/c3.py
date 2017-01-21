5a2b572c2235b440cef619189d33c01a74cba5b5
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-01-21 16:36:54 -0500

    Add Challenges 3 and 4

---
+++ challenges/c3.py
@@ -0,0 +1,16 @@
+from challenge import Challenge
+import flask
+
+class c3(Challenge):
+    '''
+    Challenge 3
+    JavaScript XOR
+    '''
+
+    def __init__(self):
+        super()
+        self._id = '0545b77581c302da4a59f52eeee4dc62904ad851'
+        self._hints = {}
+
+    def get_response(self, app):
+        return app.send_static_file('c3/index.html')
---
+++ challenges/c4.py
@@ -0,0 +1,17 @@
+from challenge import Challenge
+import flask
+
+class c4(Challenge):
+    '''
+    Challenge 4
+    Cookies
+    '''
+
+    def __init__(self):
+        super()
+        self._id = '983d09117bf1d67d10356cea9e361bf3fc216d92'
+        self._hints = {}
+
+    def get_response(self, app):
+        resp = app.send_static_file('c4/cookie.png')
+        resp.set_cookie('next_clue', '6360f685c52760b5c680948c604263cd6c68719c')
