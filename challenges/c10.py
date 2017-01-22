c916ed87cf0c45f212a89dd31eaf9bdceb04c93b
TranBrian10 <tranbrian10@gmail.com>
2017-01-21 21:59:50 -0500

    Finished challenge 10

--- challenges/c10.py
+++ challenges/c10.py
@@ -1,16 +1,16 @@
 from challenge import Challenge
 import flask
 
-class c12(Challenge):
+class c10(Challenge):
     '''
-    Challenge 12
+    Challenge 10
     Response Esoteric Languages
     '''
 
     def __init__(self):
         super()
-        self._id = '12'
+        self._id = '10'
         self._hints = {}
 
     def get_response(self, app):
-       return app.send_static_file('c12/index.html')
+       return app.send_static_file('c10/index.html')
